093216573fc409b65e78f2084d143c04e8fba3cb
Filipp Aleksandrov <falexandrov@gmail.com>
2025-04-20 17:15:38 +0300

    WEP-13 init autotests (#9)

--- backend/week_eat_planner/main.py
+++ backend/week_eat_planner/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from week_eat_planner.api.api import router
@@ -7,3 +8,7 @@ from week_eat_planner.api.auth import auth_router
 app = FastAPI(title='Week-Eat-Planner')
 app.include_router(router)
 app.include_router(auth_router)
+
+
+def start_app() -> None:
+    uvicorn.run(app, host='0.0.0.0', port=8000)
