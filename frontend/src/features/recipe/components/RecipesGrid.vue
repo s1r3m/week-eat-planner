0573fff0b6bed28ca5233d7e5e44fdd4bfdfa370
s1r3m <falexandrov@gmail.com>
2026-03-31 22:30:39 +0300

    WEP-59 adjust grid-cols count for weeks and recipes

--- frontend/src/features/recipe/components/RecipesGrid.vue
+++ frontend/src/features/recipe/components/RecipesGrid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid gap-6 grid-cols-2 lg:grid-cols-3 2xl:grid-cols-6">
+  <div class="grid gap-6 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
     <RecipePreview
       v-for="recipe in recipes"
       :key="recipe.id"
